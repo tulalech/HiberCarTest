b3fde330e65b7d0f1571d5b98e82b4cd2b0c1068
tulalech <lech.tula@onet.eu>
2021-08-10 16:31:19 +0200

    test to hibercar

---
+++ src/test/java/pl/lech/hibercar/HiberCarApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lech.hibercar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiberCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
